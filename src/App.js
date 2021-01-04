087feb84357d4c7493bead0151574e526cfc2918
Bianca da Silva Campos <biancascampos0410@gmail.com>
2021-01-04 14:59:31 -0300

    Junção back e front

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 import Header from "./components/Header";
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [projects, setProjects] = useState(["Desenvolvimento de app", "Front-end web"]);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get("projects").then((response) => {
+      setProjects(response.data)
+    });
+  }, []);
 
   function handleAddProject() {
-    setProjects([...projects,`Novo projeto ${Date.now()}`])
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
 
-    console.log(projects)
+    console.log(projects);
   }
 
   return (
     <>
       <Header title="Homepage" />
+
       <ul>
         {projects.map((project) => (
-          <li key={project}>{project}</li>
+          <li key={project.id}>{project.title}</li>
         ))}
       </ul>
 
