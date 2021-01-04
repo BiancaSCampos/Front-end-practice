f395410acd5e026c9722b0d9b92559afa01a526e
Bianca da Silva Campos <biancascampos0410@gmail.com>
2021-01-04 11:20:23 -0300

    Fitst Commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Header from "./components/Header";
+import './App.css'
+
+function App() {
+  const [projects, setProjects] = useState(["Desenvolvimento de app", "Front-end web"]);
+
+  function handleAddProject() {
+    setProjects([...projects,`Novo projeto ${Date.now()}`])
+
+    console.log(projects)
+  }
+
+  return (
+    <>
+      <Header title="Homepage" />
+      <ul>
+        {projects.map((project) => (
+          <li key={project}>{project}</li>
+        ))}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>
+        Adicionar projeto
+      </button>
+    </>
+  );
+}
+
+export default App;
